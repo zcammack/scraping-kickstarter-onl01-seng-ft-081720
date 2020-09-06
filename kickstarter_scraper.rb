4e2b0baa77ea1daf97c53a015714a2e4f7487759
Zackary Cammack <zack.cammack@gmail.com>
2020-09-06 22:44:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 
   def create_project_hash
     html = File.read('fixtures/kickstarter.html')
-    kickstarter = Nokogiri::HTML
+    kickstarter = Nokogiri::HTML(html)
     binding.pry
   end
 
