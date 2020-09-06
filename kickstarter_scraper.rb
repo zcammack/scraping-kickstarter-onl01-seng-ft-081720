c55bb42de901f561b678db18dd422a80823d8e21
Zackary Cammack <zack.cammack@gmail.com>
2020-09-06 23:28:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,9 +11,9 @@ require 'pry'
   def create_project_hash
     html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
-   
+
     projects = {}
-   
+
     kickstarter.css("li.project.grid_4").each do |project|
       title = project.css("h2.bbcard_name strong a").text
       projects[title.to_sym] = {
